3c62a61e50b14809d9bc5d3e4185e1a2e991f8ad
wen1995cc@outlook.com <wen19951225>
2019-04-02 11:54:54 +0800

    ch15Data

---
+++ python_carsh_course/ch15/scatter_squares.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.scatter(2, 4, s=200)
+plt.title("Squares Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Squares of value", fontsize=14)
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.show()
